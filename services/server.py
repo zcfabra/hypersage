381dfc4479e3df9e6b629bf08651b0754bfe79e0
zcfabra <christianwgnam@gmail.com>
2022-12-14 11:11:09 -0500

    sents

--- services/server.py
+++ services/server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, make_response
 from spacy.pipeline import EntityRecognizer
 from flask_cors import CORS
 import spacy
+import time
 app = Flask(__name__)
 
 cors = CORS()
@@ -18,16 +19,30 @@ def nerRoute():
     data = request.json["data"]
     print(data)
 
-    out = []
+    out = {}
 
     for file in data:
         text = file["file"]["text"]
         doc = nlp(text)
-        out.append({"fileID": file["file"]["id"] ,"data": [{"text": ent.text, "label": ent.label_} for ent in doc.ents]})
+        out[file["file"]["id"]] = {"data": [{"text": ent.text, "label": ent.label_} for ent in doc.ents]}
     print(out)
-    res = make_response(jsonify({"data": out}))
+    res = make_response(jsonify(out))
     res.headers["Content-Type"] = "application/json"
+    # time.sleep(10)
+    return res
 
+@app.route("/tasks/Sentiment", methods=["POST"])
+def sentimentRoute():
+    files = request.json["data"]
+    print("FILES", files)
+    ids = [file["fileID"] for file in files]
+    docs = [nlp(file["file"]["text"]) for file in files]
+
+    out = {}
+    for ix, doc in enumerate(docs):
+        out[ids[ix]] = {"score": (sent_score:=doc._.blob.polarity), "sentiment": "POS" if sent_score > 0.0 else "NEG", "pos_words": [each[0][0] for each in doc._.sentiment_assessments.assessments if each[1] > 0], "neg_words": [each[0][0] for each in doc._.sentiment_assessments.assessments if each[1] < 0]}
+    res = make_response(jsonify(out))
+    res.headers["Content-Type"] = "application/json"
     return res
 
 @app.route("/tasks/Similarity", methods=["POST"])
